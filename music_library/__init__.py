8d71cc0e68a7aeaf93b12659f8a79eb9f7e939f3
Carlos Garcia <carlos.grh@outlook.com>
2022-12-16 12:58:10 -0500

    finished organizing env files, routes.py and __init__.py for blueprint and pages

--- music_library/__init__.py
+++ music_library/__init__.py
@@ -1,14 +1,20 @@
-from flask import Blueprint, render_template
+from music_library.routes import pages
+from flask import Flask
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
 
-pages = Blueprint(
-    "pages", __name__, template_folder="templates", static_folder="static"
-)
+load_dotenv()
 
 
+def create_app():
+    app = Flask(__name__)
+    mongo_address = os.getenv("MONGODB_ADDRESS")
+    secret_key = os.getenv("SECRET_KEY")
+    app.config["MONGO_URI"] = mongo_address
+    app.config["SECRET_KEY"] = secret_key
+    app.db = MongoClient(app.config["MONGO_URI"]).get_default_database()
 
-@pages.route("/")
-def index():
-    return render_template(
-        "index.html",
-        title="Music Review List"
-    )
+    app.register_blueprint(pages)
+    return app
+
