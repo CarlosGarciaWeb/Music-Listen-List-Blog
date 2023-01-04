c2b7365305775517309c581dd199cf4a676e645c
Carlos Garcia <carlos.grh@outlook.com>
2023-01-04 18:17:03 -0500

    add feature in endpoint for adding song and display song data in index. that allows to store a class object from the model.py file of a song object.

--- music_library/routes.py
+++ music_library/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
-
-
+from flask import Blueprint, render_template, session, redirect, request, current_app, url_for
+from music_library.forms import MusicForm
+from music_library.models import Song
+import uuid
+from dataclasses import asdict
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -10,7 +12,41 @@ pages = Blueprint(
 
 @pages.route("/")
 def index():
+    song_data = current_app.db.songList.find({})
+    songs = [Song(**song) for song in song_data]
     return render_template(
         "index.html",
-        title="Music Review List"
+        title="Music Review List",
+        songs=songs
     )
+
+@pages.route("/add", methods=["GET", "POST"])
+def add_song():
+    form = MusicForm()
+
+
+    if form.validate_on_submit():
+        song = Song(
+            _id = uuid.uuid4().hex,
+            name = form.name.data,
+            author = form.author.data,
+            album = form.album.data,
+            genre = form.genre.data
+        )
+
+        current_app.db.songList.insert_one(asdict(song))
+
+        return redirect(url_for(".index"))
+
+    return render_template("new_song.html", title="Music Review List - Add Song", form=form)
+
+@pages.get("/toggle-theme")
+def toggle():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+
+    return redirect(request.args.get("current_page"))
